79e1da37b98ad0d16cda116ebca230a2fce6332d
amine7536 <amine@pixelfactory.io>
2021-11-10 15:08:31 +0100

    Brew formula update for git-get version v1.0.2-beta

--- git-get.rb
+++ git-get.rb
@@ -5,37 +5,52 @@
 class GitGet < Formula
   desc "Clone git repository in $GIT_GET_PROJECTS_PATH and preserves <githost>/<username>/<repository> structure."
   homepage "https://github.com/pixelfactoryio/git-get"
-  version "1.0.1-beta"
+  version "1.0.2-beta"
   license "MIT"
-  bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.1-beta/git-get_Darwin_x86_64.tar.gz"
-      sha256 "b3892cfc2e8b823aff7a0ed2ca25dea5393d44faa185bce541d8a6b333108f29"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.1-beta/git-get_Darwin_arm64.tar.gz"
-      sha256 "47414e3a2f682fcc548872a2e4eee793bdbc62a243e42b09858e2c436cd87527"
+      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.2-beta/git-get_Darwin_arm64.tar.gz"
+      sha256 "5af32dcb4f21550d803b64d277cde9895256c3559525dfb00ea8afd3e7fe0edd"
+
+      def install
+        bin.install "git-get"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.2-beta/git-get_Darwin_x86_64.tar.gz"
+      sha256 "76c7432fe04e9757049269f5ee48bee023b1af8e1af9a226e184cf3cc33ea219"
+
+      def install
+        bin.install "git-get"
+      end
     end
   end
 
   on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.2-beta/git-get_Linux_arm64.tar.gz"
+      sha256 "8c7e9de63215fe7fa2a95c954fb11b8904463a50dc741f4cefc1110e49e93563"
+
+      def install
+        bin.install "git-get"
+      end
+    end
     if Hardware::CPU.intel?
-      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.1-beta/git-get_Linux_x86_64.tar.gz"
-      sha256 "0d2d37c92ff1abebc9183ed13d9578f1a859b68d08ca484dc519851334ab8e1f"
+      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.2-beta/git-get_Linux_x86_64.tar.gz"
+      sha256 "156decda5696e0961795190436e5f2145deab12c65fcd482fafc543f67396b6c"
+
+      def install
+        bin.install "git-get"
+      end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.1-beta/git-get_Linux_armv6.tar.gz"
-      sha256 "b2e336854556327e841f12da749cf77499febec298ba65d4f5670d30a5b9e8cc"
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.1-beta/git-get_Linux_arm64.tar.gz"
-      sha256 "39f0834e6fc1b3192db3d7927cbf8cdb1d156bfdc0620db4a2b5ac9a26363d72"
-    end
-  end
+      url "https://github.com/pixelfactoryio/git-get/releases/download/v1.0.2-beta/git-get_Linux_armv6.tar.gz"
+      sha256 "dee4e2efd84853948d54b21db683c05283173cd0c52f676d9994c0c06f961207"
 
-  def install
-    bin.install "git-get"
+      def install
+        bin.install "git-get"
+      end
+    end
   end
 end
